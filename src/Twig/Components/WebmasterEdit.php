93f9de53c4a8aa6421191dbdc1f4e5eb5027d4ea
FOURNIER Sébastien <fournier.sebastien@outlook.com>
2026-03-14 12:32:45 +0100

    Refactor

--- src/Twig/Components/WebmasterEdit.php
+++ src/Twig/Components/WebmasterEdit.php
@@ -5,20 +5,29 @@ declare(strict_types=1);
 namespace App\Twig\Components;
 
 use App\Service\Interface\CoreLocatorInterface;
-use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 /**
  * WebmasterEdit.
  *
  * @author Sébastien FOURNIER <fournier.sebastien@outlook.com>
  */
-#[AsTwigComponent]
+#[AsLiveComponent('WebmasterEdit')]
 class WebmasterEdit
 {
+    use DefaultActionTrait;
+
+    #[LiveProp]
     public ?string $title = null;
+
+    #[LiveProp]
     public string $path = '#';
+
+    #[LiveProp]
     public ?string $role = null;
-    
+
     public function __construct(private readonly CoreLocatorInterface $coreLocator)
     {
     }
